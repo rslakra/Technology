8047760c2a2af70c331c404f823b2d1debd4b68a
Rohtash Lakra <rslakra@users.noreply.github.com>
2025-04-17 21:13:47 -0700

    removed guava dependency

--- swagger-services/swagger-sample/src/main/java/com/rslaka/swaggersample/config/SwaggerConfig.java
+++ swagger-services/swagger-sample/src/main/java/com/rslaka/swaggersample/config/SwaggerConfig.java
@@ -1,16 +1,14 @@
 package com.rslaka.swaggersample.config;
 
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static com.google.common.base.Predicates.or;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -20,29 +18,17 @@ public class SwaggerConfig {
      * @return
      */
     @Bean
-    public Docket postsApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("public-api")
-                .apiInfo(apiInfo()).select()
-                .paths(postPaths()).build();
-    }
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(apiInfo()).select().paths(PathSelectors.any()).build();
 
-    /**
-     * @return
-     */
-    private Predicate<String> postPaths() {
-        return or(regex("/api/posts.*"), regex("/api/swagger.*"));
     }
 
     /**
      * @return
      */
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Swagger API")
-                .description("Swagger API reference for developers")
-                .termsOfServiceUrl("http://swagger.com")
-                .contact("oauth@gmail.com").license("Swagger License")
-                .licenseUrl("oauth@gmail.com").version("1.0").build();
+        return new ApiInfoBuilder().title("Swagger API").description("Swagger API reference for developers").termsOfServiceUrl("http://swagger.com").contact(new Contact("Rohtash", "www.rslakra.com", "swagger@gmail.com")).license("Swagger License").licenseUrl("oauth@gmail.com").version("1.0.0").build();
     }
+
 
 }
